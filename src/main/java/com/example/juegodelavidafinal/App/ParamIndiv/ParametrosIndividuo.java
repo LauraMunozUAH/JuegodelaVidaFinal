bfbc9457beeb6160de0d33fa3af61e17667456c8
martavalientemaldonado <157739936+martavalientemaldonado@users.noreply.github.com>
2024-05-04 10:56:05 +0200

    Add files via upload

---
+++ src/main/java/com/example/juegodelavidafinal/App/ParamIndiv/ParametrosIndividuo.java
@@ -0,0 +1,5 @@
+package com.example.juegodelavidafinal.App.ParamIndiv;
+
+public class ParametrosIndividuo {
+
+}
---
+++ src/main/java/com/example/juegodelavidafinal/image/botonamarillo.java
@@ -0,0 +1,5 @@
+package com.example.juegodelavidafinal.image;
+
+public class botonamarillo {
+    public static final String  url ="..\\..\\..\\..\\..\\Downloads\\botonamarillo.PNG";
+}
